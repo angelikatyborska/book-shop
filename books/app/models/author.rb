522e622dd626b105d3f544be0742443fb859e04f
Angelika Tyborska <atyborska93@gmail.com>
2017-08-13 14:51:57 +0200

    Add book_count to author

--- books/app/models/author.rb
+++ books/app/models/author.rb
@@ -1,4 +1,14 @@
 class Author < ActiveRecord::Base
   has_many :books, dependent: :destroy
   # TODO: validations
+
+  default_scope { includes(:books) }
+
+  def book_count
+    books.length # .count causes a SELECT COUNT(*) database query
+  end
+
+  def attributes
+    super.merge(book_count: book_count)
+  end
 end
--- books/config/environment.rb
+++ books/config/environment.rb
@@ -3,4 +3,5 @@ require_relative './application'
 require 'yaml'
 require 'erb'
 
+ActiveRecord::Base.logger = Logger.new(STDOUT) # FIXME: share the logger between the database, bunny and the app
 ActiveRecord::Base.establish_connection(Books.database_config)
